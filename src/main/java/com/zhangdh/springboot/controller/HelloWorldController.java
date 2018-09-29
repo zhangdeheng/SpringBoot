c62d4f944ef40ac3dcb1ca780a7f8ee1035186b5
zhangdeheng <zhangdh@csdn.net>
2018-09-29 22:35:53 +0800

    自动根据方法生成缓存

--- src/main/java/com/zhangdh/springboot/controller/HelloWorldController.java
+++ src/main/java/com/zhangdh/springboot/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.zhangdh.springboot.controller;
 
+import com.zhangdh.springboot.dao.UserRepository;
 import com.zhangdh.springboot.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 //RestController 默认返回json 格式
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private UserRepository userRepository;
     @RequestMapping("/getUser")
+    //自动根据方法生成缓存
+    @Cacheable(value="user-key")
     public User getUser() {
-        User user=new User();
-        user.setUserName("zhangdh");
-        user.setEmail("1600976604@qq.com");
+//        User user=new User();
+//        user.setUserName("zhangdh");
+//        user.setEmail("1600976604@qq.com");
+        User user=userRepository.findByUserName("aa");
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
         return user;
     }
 }
